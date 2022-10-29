42252e8a72e0cecf20c1ae17f678971c9417e6c8
João Vitor Carvalho <joaovitortst777@gmail.com>
2022-10-29 14:50:33 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Spin Off</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="responsividade.css">
+</head>
+<body>
+    <header>
+        <a href="https://www.domainmarket.com/buynow/batatinha.com">
+            <img src="SOF.png" alt="">
+        </a>
+
+        <button>Contatos</button>
+    </header>
+
+    <main>
+        
+        <h1>Cadastro de vendas</h1>
+        <form action="" method="POST">
+                <input type="text"  name="input_nome" id="input_nome" placeholder="Cliente"> <br>
+                <input type="text" name="input_cpf" id="input_cpf" placeholder="Funcionário"><br>
+                <input type="date" name="input_cargo" id="input_cargo" placeholder="Data">
+        </form>
+
+    </main>
+
+    <footer>
+    <h2>Footer</h2>
+    </footer>
+    
+</body>
+</html>
